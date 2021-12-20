7397fd50c6abbd3336041c45d82a2903e5d2a8f2
Jerry <81377042+jlohding@users.noreply.github.com>
2021-12-20 16:14:45 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Arbitrage Against The Machine
+
+This repository contains my static quantitative finance blog/personal site built with Flask and Bootstrap.
---
+++ main.py
@@ -0,0 +1,49 @@
+import datetime as dt
+from flask import Flask, render_template, redirect
+from flask_flatpages import FlatPages
+
+app = Flask(__name__)
+app.config['FLATPAGES_EXTENSION'] = '.html' #'.md'
+pages = FlatPages(app)
+
+@app.route("/")
+@app.route("/home")
+def home_page():
+    posts = [x for x in pages if "date" in x.meta]
+    sorted_pages=sorted(posts, reverse=True, key=lambda page: dt.datetime.strptime(page.meta["date"], "%d %b %Y"))
+    return render_template("home.html", pages=sorted_pages)
+ 
+@app.route("/projects")
+def projects_page():
+    return render_template("projects.html")
+
+@app.route("/blog/")
+@app.route("/blog/<filter_tag>")
+def blog_page(filter_tag=None):
+    posts = [x for x in pages if "date" in x.meta]
+    sorted_pages=sorted(posts, reverse=True, key=lambda page: dt.datetime.strptime(page.meta["date"], "%d %b %Y"))
+
+    if filter_tag:
+        filter_pages = [page for page in sorted_pages if filter_tag in page.meta["tags"] or filter_tag in page.meta["date"]]
+    else:
+        filter_pages = sorted_pages
+
+    return render_template("blog.html", pages=sorted_pages, filter_pages=filter_pages)
+
+@app.route("/about")
+def about_page():
+    return render_template("about.html")
+
+@app.route("/blog/<path:path>.html")
+def blog_post(path):
+    page = pages.get_or_404(path)
+    return render_template("post.html", page=page)
+
+@app.route("/external_links/<site>")
+def ext_links(site):
+    sites = {
+        "github": "https://www.github.com/jlohding",
+        "arbitrade": "https://www.github.com/jlohding/arbitrade",
+        "linkedin": "https://www.linkedin.com/in/jerryloh2000",
+    }
+    return redirect(sites[site], code=302)
